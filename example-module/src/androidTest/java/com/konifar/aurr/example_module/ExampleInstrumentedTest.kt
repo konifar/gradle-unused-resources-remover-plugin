7e21e5b7abd75087956bec7ff762236842884c47
konifar <yahpeycoy0403@gmail.com>
2018-04-29 00:52:00 +0900

    Created test module

---
+++ example-module/src/androidTest/java/com/konifar/aurr/example_module/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.konifar.aurr.example_module
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.konifar.aurr.example_module.test", appContext.packageName)
+    }
+}
---
+++ example-module/src/main/java/com/konifar/aurr/example_module/MainActivityFragment.kt
@@ -0,0 +1,18 @@
+package com.konifar.aurr.example_module
+
+import android.support.v4.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+class MainActivityFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
