644d19a114b9d4b3e73f8e37b42bc42904631635
konifar <yahpeycoy0403@gmail.com>
2018-05-04 18:05:42 +0900

    Add style sample

--- example/src/main/java/com/konifar/aurr/MainActivityFragment.kt
+++ example/src/main/java/com/konifar/aurr/MainActivityFragment.kt
@@ -2,9 +2,11 @@ package com.konifar.aurr
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 /**
  * A placeholder fragment containing a simple view.
@@ -13,6 +15,12 @@ class MainActivityFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+
+        val wrappedContext = ContextThemeWrapper(context, R.style.UsedInKotlin)
+        val textView = TextView(wrappedContext, null, 0)
+
+        return view
     }
+
 }
