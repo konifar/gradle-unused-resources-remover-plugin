9422c4779e713f6b8ceda545a5d01a7f649b3665
konifar <yahpeycoy0403@gmail.com>
2018-05-04 19:25:09 +0900

    Added databinding sample

---
+++ example/src/main/java/com/konifar/aurr/DataBindingActivity.kt
@@ -0,0 +1,15 @@
+package com.konifar.aurr
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.konifar.aurr.databinding.ActivityDatabindingBinding
+
+class DataBindingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Test for generated object from layout
+        val binding = ActivityDatabindingBinding.inflate(layoutInflater)
+    }
+}
