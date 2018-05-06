bb17b576658dd65eda44523a4bcb600a456671cd
konifar <yahpeycoy0403@gmail.com>
2018-05-06 11:52:16 +0900

    Added sample

--- example/src/main/java/com/konifar/aurr/MainFragment.kt
+++ example/src/main/java/com/konifar/aurr/MainFragment.kt
@@ -3,6 +3,7 @@ package com.konifar.aurr
 import android.animation.AnimatorInflater
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
 import android.support.v7.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
@@ -18,6 +19,12 @@ class MainFragment : Fragment() {
         val wrappedContext = ContextThemeWrapper(context, R.style.UsedInKotlin)
         val textView = TextView(wrappedContext, null, 0)
         textView.setText(R.string.used_in_kotlin)
+        textView.setTextColor(ContextCompat.getColor(context!!, R.color.used_in_kotlin_color))
+
+        textView.isEnabled = resources.getBoolean(R.bool.used_in_kotlin_bool)
+
+        val padding = resources.getDimension(R.dimen.used_in_kotlin_dimen).toInt()
+        textView.setPadding(padding, padding, padding, padding)
 
         AnimatorInflater.loadAnimator(context, R.animator.used_in_kotlin_animator)
 
